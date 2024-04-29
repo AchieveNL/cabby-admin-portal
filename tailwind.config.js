15301dbd4a8aadabe750b218cc89f519d49e8f36
MrAbdul <monimesamadi@gmail.com>
2024-04-29 10:41:56 +0100

    overview changes tasks 58 and 59

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
